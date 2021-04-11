0d778fb5a4a245c0dc907e9195dd2eff56bbfb8b
luka dodelia <luka@picktek.com>
2021-04-11 12:57:39 +0400

    tests fix

--- test/support/server.js
+++ test/support/server.js
@@ -1,25 +1,27 @@
 const log     = require('metalogger')();
 const { json } = require('body-parser');
 const express = require('express');
-let app = express();
+const app = express();
 
 function responder(req, res) {
     res.send('Hello World!');
 }
 
 function jsonResponder(req, res) {
-    const input = req.body;
-    log.info("wtf", input);
-    res.json({rsv: 100});
+    res.json(req.body);
 }
 
-app.get('/', responder);
-app.get('/hello', responder);
 
-app.post('/jsonParser', jsonResponder);
-
-module.exports.getServer = () => {
+module.exports.getServer = async () => {
     const serverFactory = require('../../lib');
-    const testServer =  serverFactory.getTestServer(app);
-    return testServer;
+    return new Promise(resolve=>{
+        serverFactory.getTestServer(app,(server,app)=>{
+            app.get('/', responder);
+            app.get('/hello', responder);
+
+            app.post('/jsonParser', jsonResponder);
+
+            resolve(server);
+        });
+    });
 };
