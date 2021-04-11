f5441c8d6cb456e044186f3196e8dd136f40209a
Irakli Nadareishvili <inadarei@users.noreply.github.com>
2021-04-11 12:15:23 -0400

    fixing tests hanging

--- test/acceptance/basic-test.js
+++ test/acceptance/basic-test.js
@@ -3,8 +3,16 @@ const nf = require('node-fetch');
 const log  = require('metalogger')();
 const fp = require('fakepromise');
 const express = require('express');
+const http= require('http');
 
 const nb_server = require('../../lib');
+process.env.NODE_CLUSTERED = 0;
+
+test('Empty test to check setup', async t => {
+  const res = await nf("https://api.publicapis.org/entries"); 
+  const data = await res.json();
+  t.same(data.count, 730);
+});
 
 test('Basic Express Response', async t => {
 
@@ -13,21 +21,19 @@ test('Basic Express Response', async t => {
   };
 
   const app  = nb_server.setupTest();
-  app.get('/', responder);
   app.get('/hello', responder);
-
-  const server  = nb_server.getTestServer(app);
-  const baseuri = nb_server.serverUri(server);
+  const baseuri = nb_server.serverUri(app.http_server);
 
   try {
     const res = await nf(`${baseuri}/hello`);  
     t.equal(res.status, 200, 'proper http status code for /hello');
     t.same(await res.text(), 'Hello World!',
            'proper response payload for /hello');  
+    //t.same(1,1);
   } catch (err) {
     t.fail(err);
   } finally {
-    server.close();
+    app.http_server.close();
   }
   
   
@@ -38,14 +44,12 @@ test('Ability To Process JSON Input', async t => {
   const jsonResponder = (req, res) => {
     // This is what verified that body gets parsed!
     const input = req.body;
-    res.json({rsv: input.rsv}).end();
+    res.json({rsv: input.rsv});
   };
 
   const app  = nb_server.setupTest();
   app.post('/jsonParser', jsonResponder);
-
-  const server  = nb_server.getTestServer(app);
-  const baseuri = nb_server.serverUri(server);
+  const baseuri = nb_server.serverUri(app.http_server);
 
   try {    
     const payload = {rsv: 100, anotherField: "blahblah", "oh" : "yes"};
@@ -63,7 +67,7 @@ test('Ability To Process JSON Input', async t => {
   } catch (err) {
     t.fail(err);
   } finally {
-    server.close();
+    app.http_server.close();
   }
   
 });
